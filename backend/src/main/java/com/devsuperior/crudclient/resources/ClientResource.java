d6e2d8c05000db30e487b678f45bc3f13a316011
Anderson Pinto <anderson.p20@gmail.com>
2022-05-06 19:51:32 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/crudclient/resources/ClientResource.java
@@ -1,15 +1,15 @@
 package com.devsuperior.crudclient.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.crudclient.entities.Client;
+import com.devsuperior.crudclient.services.ClientService;
 
 
 
@@ -17,11 +17,12 @@ import com.devsuperior.crudclient.entities.Client;
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Alberto Silva", "000.000.000-00", 5000.00,
-				Instant.parse("2020-07-13T20:50:07.12345Z"), 0));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
---
+++ backend/src/main/java/com/devsuperior/crudclient/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.crudclient.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.crudclient.entities.Client;
+import com.devsuperior.crudclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll(){
+		return repository.findAll();
+	}
+
+}
